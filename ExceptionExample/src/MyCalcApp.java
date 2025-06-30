00fd26fb2053600c9c7bda354d5895e0671423d8
sridevi-dns <sridevi.dns@gmail.com>
2025-06-30 17:29:00 +0530

    Exception Example without try-catch

--- ExceptionExample/src/MyCalcApp.java
+++ ExceptionExample/src/MyCalcApp.java
@@ -8,13 +8,10 @@ public class MyCalcApp {
         int number = scanner.nextInt();
 
         Calculator calculator = new Calculator();
-        try {
+
             double doubleNum =calculator.caldouble(number);
             System.out.println("Double of "+ number +" is "+ doubleNum);
-        }
-     catch (MyArithException ex){
-         System.out.println(ex.getMessage());
-     }
+
         scanner.close();
     }
 }
