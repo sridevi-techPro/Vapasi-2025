11405399feb96745cbbbd8379857567dec3ede57
sridevi-dns <sridevi.dns@gmail.com>
2025-07-04 00:24:58 +0530

    ExecutorService with a fixed thread pool to simulate handling 5 ticket booking requests.

--- TicketBookingService/src/main/java/com/tw/ticketbooking/TicketBookingJob.java
+++ TicketBookingService/src/main/java/com/tw/ticketbooking/TicketBookingJob.java
@@ -6,24 +6,24 @@ class TicketBookingJob implements Runnable {
     private static final Logger logger = Logger.getLogger(TicketBookingJob.class.getName());
     private static final String WHITE = "\u001B[37m";
     private static final String RESET = "\u001B[0m";
-    private final int bookingId;
+    private final String movieGoer;
 
-    public TicketBookingJob(int bookingId) {
-        this.bookingId = bookingId;
+    public TicketBookingJob(String bookingName) {
+        this.movieGoer = bookingName;
     }
 
 
     @Override
     public void run() {
         try {
-            logger.info(WHITE + "Booking ID " + bookingId + ": Booking received by " + Thread.currentThread().getName() + RESET);
+            logger.info(WHITE + movieGoer + " books a ticket. Their Booking is received by " + Thread.currentThread().getName() + RESET);
             Thread.sleep(1000);
 
-            logger.info(WHITE + "Booking ID " + bookingId + ": Payment processed by " + Thread.currentThread().getName() + RESET);
+            logger.info(WHITE + movieGoer + " pays for the ticket. Their Payment is processed by " + Thread.currentThread().getName() + RESET);
             Thread.sleep(1000);
 
-            logger.info(WHITE + "Booking ID " + bookingId + ": Ticket confirmed by " + Thread.currentThread().getName() + RESET);
-
+            logger.info(WHITE + movieGoer + " receives the Ticket. Their Ticket is confirmed by " + Thread.currentThread().getName() + RESET);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         } catch (Exception e) {
--- TicketBookingService/src/main/java/com/tw/ticketbooking/TicketBookingService.java
+++ TicketBookingService/src/main/java/com/tw/ticketbooking/TicketBookingService.java
@@ -1,5 +1,7 @@
 package com.tw.ticketbooking;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -8,9 +10,9 @@ public class TicketBookingService {
         int numThreads = 3;
         try (ExecutorService executor = Executors.newFixedThreadPool(numThreads)) {
 
-            int totalBookings = 5;
-            for (int i = 1; i <= totalBookings; i++) {
-                executor.execute(new TicketBookingJob(i));
+            List<String> MovieGoers = Arrays.asList("1. Alice", "2. Bob", "3. Charlie","4. Danny","5. Emma");
+            for (int i = 1; i < MovieGoers.size(); i++) {
+                executor.execute(new TicketBookingJob(MovieGoers.get(i)));
             }
 
             executor.shutdown();
